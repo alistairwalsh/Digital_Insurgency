7220ddae831a680708ed71469de4e6be674d830a
Alistair Walsh <alistair.walsh@me.com>
2023-05-20 09:04:12 +1000

    add nisha video

--- pages/1_Nisha_Nakamura.py
+++ pages/1_Nisha_Nakamura.py
@@ -5,3 +5,8 @@ st.title('Nisha Nakamura')
 st.image('images/Nisha_Nakamura.jpg')
 with open('text/Nisha_Nakamura.txt') as infile:
     st.write(infile.read())
+
+video_file = open('video\Nisha.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
