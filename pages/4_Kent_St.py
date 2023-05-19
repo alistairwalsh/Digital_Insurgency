ce9006f486d755e49d351a605cc9ab919e6c61b5
Alistair Walsh <alistair.walsh@me.com>
2023-05-20 09:10:19 +1000

    added other vids

--- pages/4_Kent_St.py
+++ pages/4_Kent_St.py
@@ -21,4 +21,8 @@ st.image('images/Lena.jpg')
 with open('text/lena.txt') as infile:
     st.write(infile.read())
 
+with open('video/Lena, the bar_s resident DJ.mp4', 'rb') as infile:
+    video_bytes = infile.read()
+    st.video(video_bytes)
+
 
--- pages/5_Scene_1_the night_is_young.py
+++ pages/5_Scene_1_the night_is_young.py
@@ -6,3 +6,7 @@ st.image('images/Scene_1.jpg')
 
 with open('text/scene_1.txt') as infile:
     st.write(infile.read())
+
+with open('video/Scene Setting_ Kent Street Dive Bar, bathed in dim neon lights, throbs with an electric atmosphere. Nisha, beat from her recent runs, seeks solace in -2 (1684566823898).mp4', 'rb') as infile:
+    video_bytes = infile.read()
+    st.video(video_bytes)
