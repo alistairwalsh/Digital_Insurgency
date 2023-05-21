91b39786c66bdbbdab34a6dd397a93310c1425bb
alistairwalsh <alistairwalsh@me.com>
2023-05-21 17:20:29 +1000

    added about

---
+++ pages/about.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('About this site')
+
+st.write("I know I need to offer some explanation about what this is, and I'm working on it.")
+st.write("In case it hasn't been obvious to you up until now, this site is continuously changeing as it develops")
+st.write("I asked chat-gpt to create a world based on the writings of my favourite author - William Gibson")
+st.write("I didn't write any of the text or create any of the images or videos, that is the output of the AI models. My only input is to prompt and direct the AI")
