585221ad2d71ff93748a44a11b97632d1683ee43
alistairwalsh <alistairwalsh@me.com>
2023-05-21 17:24:51 +1000

    message

--- pages/about.py
+++ pages/about.py
@@ -6,3 +6,6 @@ st.write("I know I need to offer some explanation about what this is, and I'm wo
 st.write("In case it hasn't been obvious to you up until now, this site is continuously changeing as it develops")
 st.write("I asked chat-gpt to create a world based on the writings of my favourite author - William Gibson")
 st.write("I didn't write any of the text or create any of the images or videos, that is the output of the AI models. My only input is to prompt and direct the AI")
+st.write("The next thing I want to impliment is a comments section and some way for anybody to interact with the AI and create this story - not sure how to impliment this but I'm working on it.")
+st.write(" I will try to get the comments section going first so there is a place for interaction with anyone who finds this")
+st.write(" after that I will put inplace some way for you to interact with the AI and shape this world")
