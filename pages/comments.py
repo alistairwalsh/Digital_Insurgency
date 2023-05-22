07ed091f9b3dc52fabee66e725b8ce1574ac9254
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 12:31:21 +1000

    added url

--- pages/comments.py
+++ pages/comments.py
@@ -1,3 +1,9 @@
 from streamlit_disqus import st_disqus
 
-st_disqus("streamlit-disqus-demo")
+st_disqus(shortname="streamlit-disqus-demo",
+    url='https://alistairwalsh-digital-insurgency-digital-insurgency-1d0lze.streamlit.app/comments',
+    identifier='comments',
+    title=None,
+    category_id=None,
+    language=None,
+    key=None)
