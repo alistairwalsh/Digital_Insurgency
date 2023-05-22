79549155d7917c5b67052fa3b8c9368f39ad05cc
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 20:22:41 +1000

    added chat iframe

--- pages/4_Kent_St.py
+++ pages/4_Kent_St.py
@@ -26,6 +26,8 @@ with open('video/jackson_message.mp4', 'rb') as infile:
 with open('text/jackson_chat.txt') as infile:
     st.markdown(infile.read())
 
+components.iframe(src='https://www.kubee.ai/chat?isChange=true&userId=1660580962428149760')
+
 st.title('Lena')
 st.image('images/Lena.jpg')
 with open('text/lena.txt') as infile:
