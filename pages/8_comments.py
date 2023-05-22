6bae59547d822c445935b2368f00cda1aabbc5ae
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 18:42:00 +1000

    typo

--- pages/8_comments.py
+++ pages/8_comments.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 components.iframe(src="https://docs.google.com/forms/d/e/1FAIpQLSfs4yDOBtcmz2z3n9ameHrA04L2C5AEY1MiXiuWxE68cRcEEg/viewform?embedded=true", width=640, height=584)
 
 
-component.iframe(src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTGS7C9k_UgkK5sTJQuZLQq3LmmmFfe4i5VPAjMBehfFORy4t6DxIGsGwFv65Hy0netMjtqWjA-fbIy/pubhtml?gid=1980846108&amp;single=true&amp;widget=true&amp;headers=false")
+components.iframe(src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTGS7C9k_UgkK5sTJQuZLQq3LmmmFfe4i5VPAjMBehfFORy4t6DxIGsGwFv65Hy0netMjtqWjA-fbIy/pubhtml?gid=1980846108&amp;single=true&amp;widget=true&amp;headers=false")
 
 
 #https://forms.gle/s5fk3z9qjECU8SMB7
