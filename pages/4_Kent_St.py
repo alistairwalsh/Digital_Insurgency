e40d9261509f6097ffa28be18a1c17894fec9942
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 20:26:32 +1000

    removed excess

--- pages/4_Kent_St.py
+++ pages/4_Kent_St.py
@@ -4,7 +4,6 @@ import streamlit.components.v1 as components
 st.title('Kent St')
 
 st.image('images/kent_st.jpg')
-st.image('images/realistic_cyberpunk_fusion_blending_futuristic_el.jpg')
 components.iframe(src="https://renderstuff.com/tools/360-panorama-web-viewer-embed/?image=https://i.imgur.com/SdkOUAq.jpg", width=1200, height=700)
 components.iframe(src="https://renderstuff.com/tools/360-panorama-web-viewer-embed/?image=https://i.imgur.com/U4PIoQP.jpg", width=1200, height=700)
 with open('text/the_scene.txt') as infile:
@@ -22,9 +21,6 @@ with open('text/Jackson.txt') as infile:
 with open('video/jackson_message.mp4', 'rb') as infile:
     video_bytes = infile.read()
     st.video(video_bytes)
-
-with open('text/jackson_chat.txt') as infile:
-    st.markdown(infile.read())
 
 components.iframe(src='https://www.kubee.ai/chat?isChange=true&userId=1660580962428149760',  width=800, height=800)
 
