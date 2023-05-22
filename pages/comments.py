381b2a1913b26791209a571722136f1abc3e72fb
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 12:42:28 +1000

    try this

--- pages/comments.py
+++ pages/comments.py
@@ -3,13 +3,18 @@ import streamlit as st
 
 #adding a single-line text input widget
 
+all_comments = []
+
 name = st.text_input('Enter your name: ', 'name')
 comment = st.text_input('What do you think?: ', 'comment')
+
+all_comments.append((name,comment))
 
 
 
 
 #displaying the entered text
-
-st.write( name + ':')
-st.write(comment)
+if all_comments:
+    for n,c in all_comments:
+        st.write( n + ':')
+        st.write(c)
