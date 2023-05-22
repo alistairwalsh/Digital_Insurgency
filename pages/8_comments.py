0c0f00d8bdb2358273664f996ebc85b51d5cc687
Alistair Walsh <alistair.walsh@me.com>
2023-05-22 21:32:59 +1000

    tip me

--- pages/8_comments.py
+++ pages/8_comments.py
@@ -8,16 +8,5 @@ components.iframe(src="https://docs.google.com/forms/d/e/1FAIpQLSfs4yDOBtcmz2z3n
 components.iframe(src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTGS7C9k_UgkK5sTJQuZLQq3LmmmFfe4i5VPAjMBehfFORy4t6DxIGsGwFv65Hy0netMjtqWjA-fbIy/pubhtml?gid=1980846108&amp;single=true&amp;widget=true&amp;headers=false")
 
 
-#https://forms.gle/s5fk3z9qjECU8SMB7
+st.markdown('If you enjoy this site and want to tip me to help with running costs [Buy me a beer](https://ko-fi.com/memebrain)')
 
-
-# @st.cache_data(ttl=600)
-# def load_data(sheets_url):
-#     csv_url = sheets_url.replace('/edit#gid=', '/export?format=csv&gid=')
-#     return pd.read_csv(csv_url)
-
-
-# # ok let's load the data
-# questions_df = load_data(st.secrets["public_gsheets_url"])
-
-# https://docs.google.com/spreadsheets/d/1z6JE4_lLDqZUq77cPhq80eybiL3ZpZ7ya7x0aX14ghQ/edit?usp=sharing
