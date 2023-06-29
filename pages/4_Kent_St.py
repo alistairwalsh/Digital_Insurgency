af2a1a29e75d913c3c27fc7456f15592bc96fbcd
Alistair Walsh <alistair.walsh@me.com>
2023-06-29 13:18:00 +1000

    Update 4_Kent_St.py

--- pages/4_Kent_St.py
+++ pages/4_Kent_St.py
@@ -8,7 +8,10 @@ components.iframe(src="https://renderstuff.com/tools/360-panorama-web-viewer-emb
 components.iframe(src="https://renderstuff.com/tools/360-panorama-web-viewer-embed/?image=https://i.imgur.com/U4PIoQP.jpg", width=1200, height=700)
 
 #components.iframe(src='https://genny.lovo.ai/share/1f9f085d-90ac-4324-9bcf-c6f621267b61', height=400) 
-components.iframe(src='../2023-06-29_13-07-00.mp3', height=400)
+#components.iframe(src='../2023-06-29_13-07-00.mp3', height=400)
+
+st.audio('../2023-06-29_13-07-00.mp3')
+
 with open('text/the_scene.txt') as infile:
     st.write(infile.read())
 st.title('Max')
