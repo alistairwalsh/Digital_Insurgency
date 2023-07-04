0ed9f67b071d7e5012ea1fd70ac0c5622b4c4860
alistairwalsh <alistairwalsh@me.com>
2023-07-04 13:08:50 +1000

    reposition

--- pages/2_Ren_Hayashi.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title('Ren Hayashi')
-
-st.image('images/Ren_Hayashi.jpg')
-with open('text/Ren Hayashi.txt') as infile:
-    st.write(infile.read())
--- pages/3_Gabriel Thorn.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title('Gabriel Thorn')
-
-st.image('images/Gabriel_Thorn.jpg')
-with open('text/Gabriel Thorn.txt') as infile:
-    st.write(infile.read())
