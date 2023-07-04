042951a8d58d64c75f4ae437a063cf9d8d1c6d89
alistairwalsh <alistairwalsh@me.com>
2023-07-04 13:45:03 +1000

    remove links

--- pages/4_ChatGPT.py
+++ pages/4_ChatGPT.py
@@ -3,8 +3,6 @@ import streamlit as st
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
