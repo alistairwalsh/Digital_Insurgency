63fba11069ea3004e390107f556f0beaa7e27dea
alistairwalsh <alistairwalsh@me.com>
2023-07-04 13:28:24 +1000

    add chainlit

--- Digital_Insurgency.py
+++ Digital_Insurgency.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import chainlit as cl
+
+
+@cl.on_message
+async def main(message: str):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message}",
+    ).send()
 
 st.image('images/Digital_Insurgency.jpg')
 
