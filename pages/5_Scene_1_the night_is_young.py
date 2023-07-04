aa05a5c3c5faee3950c4a9ea93b41c72c08b5be4
Alistair Walsh <alistair.walsh@me.com>
2023-07-04 10:00:24 +1000

    Update 5_Scene_1_the night_is_young.py

--- pages/5_Scene_1_the night_is_young.py
+++ pages/5_Scene_1_the night_is_young.py
@@ -10,3 +10,6 @@ with open('text/scene_1.txt') as infile:
 with open('video/Scene Setting_ Kent Street Dive Bar, bathed in dim neon lights, throbs with an electric atmosphere. Nisha, beat from her recent runs, seeks solace in -2 (1684566823898).mp4', 'rb') as infile:
     video_bytes = infile.read()
     st.video(video_bytes)
+
+st.image('images/DreamShaper_v5_Scene_Setting_Kent_Street_Dive_Bar_bathed_in_di_0.jpg')
+
