da32dd837c8094bc0a4e4625cdb13ddcef041f79
alistairwalsh <alistairwalsh@me.com>
2023-07-07 15:48:08 +1000

    update sidebar

--- pages/5_Stable_Diffusion.py
+++ pages/5_Stable_Diffusion.py
@@ -26,12 +26,10 @@ with st.sidebar:
 
 #if stability_api_key:
     #st.text(stability_api_key)
-
-with st.sidebar:
      # Selectbox
-     st.image('images/well_built_caucasian_angel_sit_0.jpg')
-     add_selectbox = st.sidebar.selectbox(
-     "Prompt examples",
+    st.image('images/well_built_caucasian_angel_sit_0.jpg')
+    add_selectbox = st.sidebar.selectbox(
+    "Prompt examples",
        (
           "None",
            "Van Gogh painting with squirrels eating nuts", 
@@ -44,5 +42,5 @@ with st.sidebar:
            "Albert Einstein with a goofy smile",
            "Nottingham Forest football team lifting the FA Cup"
         ), index=0)
-     st.markdown('Use the above drop down box to generate _prompt_ examples')
-     st.text('Application by Gary Hutson')
+    st.markdown('Use the above drop down box to generate _prompt_ examples')
+    st.text('Application by Gary Hutson')
