7410ef939c5ca259bddf396eb79edebd3e090a0d
alistairwalsh <alistairwalsh@me.com>
2023-07-07 15:45:57 +1000

    prompt selection

--- pages/5_Stable_Diffusion.py
+++ pages/5_Stable_Diffusion.py
@@ -26,3 +26,23 @@ with st.sidebar:
 
 #if stability_api_key:
     #st.text(stability_api_key)
+
+with st.sidebar:
+     # Selectbox
+     st.image('images/well_built_caucasian_angel_sit_0.jpg')
+     add_selectbox = st.sidebar.selectbox(
+     "Prompt examples",
+       (
+          "None",
+           "Van Gogh painting with squirrels eating nuts", 
+           "Homer Simpson on a computer wearing a space suit",
+           "Mona Lisa with headphones on",
+           "A digital artwork of a roborovski hamster dressed in blue wizards clothing casting a thunderstorm with lightning",
+           "A model wearing a scuba diving suit",
+           "A looney tunes character driving a car",
+           "Optimus Prime on top of a surf board",
+           "Albert Einstein with a goofy smile",
+           "Nottingham Forest football team lifting the FA Cup"
+        ), index=0)
+     st.markdown('Use the above drop down box to generate _prompt_ examples')
+     st.text('Application by Gary Hutson')
