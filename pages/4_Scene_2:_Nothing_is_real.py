dd37b0617eba6dd96f874b09b845c49adb25a95f
Alistair Walsh <alistair.walsh@me.com>
2023-07-18 12:19:47 +1000

    Update 4_Scene_2:_Nothing_is_real.py

--- pages/4_Scene_2:_Nothing_is_real.py
+++ pages/4_Scene_2:_Nothing_is_real.py
@@ -4,5 +4,9 @@ st.title('Scene 2: Nothing is real')
 
 st.image('images/Leonardo_Diffusion_realistic_photo_of_an_angel_with_dark_glass_1.jpg')
 
+with open('video/combined_bitjammer.mp4', 'rb') as infile:
+    video_bytes = infile.read()
+    st.video(video_bytes)
+
 with open('text/scene_2.txt') as infile:
     st.write(infile.read())
