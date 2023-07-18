195dd43cf842e1a0c6689a2ae73812eeb7faf887
Alistair Walsh <alistair.walsh@me.com>
2023-07-18 12:49:16 +1000

    Update 4_Scene_2:_Nothing_is_real.py

--- pages/4_Scene_2:_Nothing_is_real.py
+++ pages/4_Scene_2:_Nothing_is_real.py
@@ -8,11 +8,5 @@ with open('video/combined_bitjammer.mp4', 'rb') as infile:
     video_bytes = infile.read()
     st.video(video_bytes)
 
-<video controls width="250" autoplay="true" muted="true" loop="true">
-<source 
-            src="video/combined_bitjammer.mp4" 
-            type="video/mp4" />
-</video>
-
 with open('text/scene_2.txt') as infile:
     st.write(infile.read())
