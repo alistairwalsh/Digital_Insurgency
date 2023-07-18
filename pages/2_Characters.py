cff8329cda04acc45fd8df2b027218e186263341
alistairwalsh <alistairwalsh@me.com>
2023-07-18 10:21:30 +1000

    bitjammer

--- pages/2_Characters.py
+++ pages/2_Characters.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-tab1, tab2, tab3, tab4 = st.tabs(["Nisha Nakamura", "Ren Hayashi", "Gabriel Thorn", "Amelia Rivers"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Nisha Nakamura", "Ren Hayashi", "Gabriel Thorn", "Amelia Rivers", 'Xavier "Bitjammer" Voss'])
 
 with tab1:
     st.title('Nisha Nakamura')
@@ -45,3 +45,19 @@ with tab4:
 
     with open("text/Amelia Rivers.txt") as infile:
         st.write(infile.read())
+
+with tab5:
+    st.title('Xavier "Bitjammer" Voss')
+    st.image('images/SDXL_09_Scene_Setting_Kent_Street_Dive_Bar_bathed_in_sunlight_0.jpg')
+
+    st.write('Bitjammer sits at the entrance, he is wearing his AI image rig mounted to his chest to capture images of the patrons.' )
+
+    st.image('images/PXL_20230716_050909165.jpg',width=300)
+
+    st.write("Once again Thankyou for letting me use your image, .")
+
+    with open("text/Xavier Bitjammer Voss.txt") as infile:
+        st.write(infile.read())
+
+
+
