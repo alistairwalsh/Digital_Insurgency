7b6612dfbb31097faf5e13a208817e534fe5078d
Alistair Walsh <alistair.walsh@me.com>
2023-07-18 23:58:12 +1000

    Update 4_Scene_2:_Nothing_is_real.py

--- pages/4_Scene_2:_Nothing_is_real.py
+++ pages/4_Scene_2:_Nothing_is_real.py
@@ -11,6 +11,6 @@ with open('video/combined_bitjammer.mp4', 'rb') as infile:
 with open('text/scene_2.txt') as infile:
     st.write(infile.read())
 
-with open('video/Nisha, adorned with cybernetic enhancements and a hardened gaze. stands in front of a grimy mirror. wearing nothing but her insecurities. She takes a (1689687422266).mp4') as infile:
+with open('video/Nisha_rap_video.mp4') as infile:
     video_bytes = infile.read()
     st.video(video_bytes)
