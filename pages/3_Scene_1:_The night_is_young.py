b639c4e9703398a7704ab23ef6b9bccf187cf743
alistairwalsh <alistairwalsh@me.com>
2023-07-18 10:03:09 +1000

    fix hugging face

--- pages/3_Scene_1:_The night_is_young.py
+++ pages/3_Scene_1:_The night_is_young.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.title('The night is young')
 
@@ -21,9 +22,4 @@ st.image('images/PXL_20230702_065835869.PORTRAIT.jpg',width=300)
 
 st.write("Thankyou for letting me use your image, I was so focussed on what I was doing I forgot to get your name - say hi in the comments and I'll add your name to the character. :-).")
 
-<iframe
-	src="https://memebrain-influx.hf.space"
-	frameborder="0"
-	width="850"
-	height="450"
-></iframe>
+components.iframe("https://memebrain-influx.hf.space")
