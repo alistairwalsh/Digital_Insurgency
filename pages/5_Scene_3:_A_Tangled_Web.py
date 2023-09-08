f84abd3ea1038813e54b2efa08506ac48a2bbb76
Alistair Walsh <alistair.walsh@me.com>
2023-09-09 01:39:21 +1000

    Update 5_Scene_3:_A_Tangled_Web.py

--- pages/5_Scene_3:_A_Tangled_Web.py
+++ pages/5_Scene_3:_A_Tangled_Web.py
@@ -8,6 +8,6 @@ st.image('images/tatiana3.png')
 with open('text/scene_3.txt') as infile:
     st.write(infile.read())
 
-st.video('https://youtu.be/kRtV9rHn-mo')
+st.video('https://www.tiktok.com/@real_memebrain/video/7276459404470308097?is_from_webapp=1&sender_device=pc&web_id=7235988086685337089')
 
 st.image('images/tatiana4.png')
