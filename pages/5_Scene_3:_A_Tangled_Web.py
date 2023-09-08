f31bc7ecd2c2657dadf4ced4f256e405b39f3d0a
Alistair Walsh <alistair.walsh@me.com>
2023-09-09 00:58:16 +1000

    Update 5_Scene_3:_A_Tangled_Web.py

--- pages/5_Scene_3:_A_Tangled_Web.py
+++ pages/5_Scene_3:_A_Tangled_Web.py
@@ -3,6 +3,9 @@ import streamlit.components.v1 as components
 
 st.title('A Tangled Web')
 
+with open('text/scene_3.txt') as infile:
+    st.write(infile.read())
+
 
 
 st.video('https://youtu.be/kRtV9rHn-mo')
