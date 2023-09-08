59d68a10c5f5f840b66eeaddcbd975ee26fafe07
Alistair Walsh <alistair.walsh@me.com>
2023-09-09 01:00:16 +1000

    Update 5_Scene_3:_A_Tangled_Web.py

--- pages/5_Scene_3:_A_Tangled_Web.py
+++ pages/5_Scene_3:_A_Tangled_Web.py
@@ -3,6 +3,8 @@ import streamlit.components.v1 as components
 
 st.title('A Tangled Web')
 
+st.image('images/tatiana3.jpg')
+
 with open('text/scene_3.txt') as infile:
     st.write(infile.read())
 
