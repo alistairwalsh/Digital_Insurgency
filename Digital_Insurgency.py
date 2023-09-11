a793972ffccecfa7632d47c5bab3b29fd22135fb
Alistair Walsh <alistair.walsh@me.com>
2023-09-11 21:02:42 +1000

    Update Digital_Insurgency.py

--- Digital_Insurgency.py
+++ Digital_Insurgency.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-[<img src="http://www.google.com.au/images/nav_logo7.png">](http://google.com.au/)
+[<img src="images/tiktok-logo-on-transparent-background-free-vector.jpg">](https://www.tiktok.com/@real_memebrain)
 
 st.header('Digital Insurgency', divider='grey')
 st.markdown("#### Unleashing the System's Edge")
