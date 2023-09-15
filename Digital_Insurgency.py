75c4986cbebb28ba90a6932c68866ac5cb7ccd6f
Alistair Walsh <alistair.walsh@me.com>
2023-09-15 04:49:51 +0000

    Update Digital_Insurgency.py

--- Digital_Insurgency.py
+++ Digital_Insurgency.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
+st.markdown("[![](images/tiktok-logo-on-transparent-background-free-vector.jpg)](https://www.tiktok.com/@real_memebrain)")
+st.markdown("[![](images/youtube.png)](https://www.youtube.com/channel/UCQv62qXnRifydR7BGksTjMQ)")
 
-with col1:
+#col1, col2 = st.columns(2)
+
+#with col1:
     #st.markdown('[![](images/tiktok-logo-on-transparent-background-free-vector.jpg)](https://www.tiktok.com/@real_memebrain)')
-    st.markdown("[![](images/tiktok-logo-on-transparent-background-free-vector.jpg)](https://www.tiktok.com/@real_memebrain)")
+    #st.markdown("[![](images/tiktok-logo-on-transparent-background-free-vector.jpg)](https://www.tiktok.com/@real_memebrain)")
     
-with col2:
+#with col2:
     #st.markdown('[![](images/download (2).png)](https://www.youtube.com/channel/UCQv62qXnRifydR7BGksTjMQ)')
-    st.markdown("[![](images/youtube.png)](https://www.youtube.com/channel/UCQv62qXnRifydR7BGksTjMQ)")
+    #st.markdown("[![](images/youtube.png)](https://www.youtube.com/channel/UCQv62qXnRifydR7BGksTjMQ)")
     
 
 #st.markdown(images/tiktok-logo-on-transparent-background-free-vector.jpg, https://www.tiktok.com/@real_memebrain)
