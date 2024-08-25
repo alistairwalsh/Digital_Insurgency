cae0047fd9cabe8def4ba4a38614017ef4f391e3
alistairwalsh <alistair.walsh@me.com>
2024-08-25 13:32:35 +1000

    remove image

--- pages/3_Scene_1:_The night_is_young.py
+++ pages/3_Scene_1:_The night_is_young.py
@@ -20,7 +20,7 @@ with open('video/Scene Setting_ Kent Street Dive Bar, bathed in dim neon lights,
 
 st.write('Sitting at the bar is a young girl with captivating eyes, lost in thought.' )
 
-st.image('images/PXL_20230702_065835869.PORTRAIT.jpg',width=300)
+#st.image('images/PXL_20230702_065835869.PORTRAIT.jpg',width=300)
 
 st.write("Thankyou for letting me use your image, I was so focussed on what I was doing I forgot to get your name - say hi in the comments and I'll add your name to the character. :-).")
 
