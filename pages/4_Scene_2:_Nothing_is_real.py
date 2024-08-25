2bfc6e41637305616183c382732ef870893befab
alistairwalsh <alistair.walsh@me.com>
2024-08-25 13:30:21 +1000

    remove photo

--- pages/4_Scene_2:_Nothing_is_real.py
+++ pages/4_Scene_2:_Nothing_is_real.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Scene 2: Nothing is real')
 
-st.image('images/Leonardo_Diffusion_realistic_photo_of_an_angel_with_dark_glass_1.jpg')
+#st.image('images/Leonardo_Diffusion_realistic_photo_of_an_angel_with_dark_glass_1.jpg')
 
 with open('video/combined_bitjammer.mp4', 'rb') as infile:
     video_bytes = infile.read()
