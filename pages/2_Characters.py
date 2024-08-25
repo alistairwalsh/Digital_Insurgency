bd4a2ac21346debc2ccfaaa703cc45a212471297
alistairwalsh <alistair.walsh@me.com>
2024-08-25 15:19:28 +1000

    extra image

--- pages/2_Characters.py
+++ pages/2_Characters.py
@@ -49,7 +49,6 @@ for character in characters:
             st.write('Sitting at the bar is a young girl with captivating eyes, lost in thought.')
             st.image('images/character_headshots/amelia_original.jpg', width=300)
             st.image('images/character_headshots/amelia_3.jpg', width=300)
-            st.image('images/character_headshots/amelia_2.jpg', width=300)
             st.write("Thank you for letting me use your image. I was so focused on what I was doing I forgot to get your name - say hi in the comments and I'll add your name to the character. :-).")
 
         if character == 'Xavier_Bitjammer_Voss':
