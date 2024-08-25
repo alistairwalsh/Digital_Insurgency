60c3f00c263ffc9bf96c52f18438b2d2fd92a642
alistairwalsh <alistair.walsh@me.com>
2024-08-25 15:40:42 +1000

    minor change

--- pages/2_Characters.py
+++ pages/2_Characters.py
@@ -58,6 +58,6 @@ for character in characters:
             st.write("Once again, thank you for letting me use your image.")
 
         if character == 'Tatiana':
-            st.image('images/character_headshots/Tatiana2.png')
+            #st.image('images/character_headshots/Tatiana2.png')
             st.image('images/character_headshots/Tatiana3.png')
             st.image('images/character_headshots/Tatiana4.png')
