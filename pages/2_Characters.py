0f1fbe2569ffbcd503cca9dbb9049ff794dd3be6
alistairwalsh <alistair.walsh@me.com>
2024-08-25 13:15:05 +1000

    move sections

--- pages/2_Characters.py
+++ pages/2_Characters.py
@@ -52,10 +52,13 @@ for character in characters:
             st.image('images/character_headshots/amelia_4.jpg')
             st.write('Sitting at the bar is a young girl with captivating eyes, lost in thought.')
             st.image('images/character_headshots/amelia_original.jpg', width=300)
+            st.image('images/character_headshots/amelia_3.jpg', width=300)
+            st.image('images/character_headshots/amelia_2.jpg', width=300)
             st.write("Thank you for letting me use your image. I was so focused on what I was doing I forgot to get your name - say hi in the comments and I'll add your name to the character. :-).")
 
         if character == 'Xavier "Bitjammer" Voss':
-            st.image('images/character_headshots/amelia_3.jpg')
+            st.image('images/character_headshots/bitjammer.jpg')
             st.write('Bitjammer sits at the entrance, he is wearing his AI image rig mounted to his chest to capture images of the patrons.')
-            st.image('images/PXL_20230716_050909165.jpg', width=300)
+            st.image('images/character_headshots/bitjammer_2.jpg', width=300)
+            st.image('images/character_headshots/bitjammer_original.jpg', width=300)
             st.write("Once again, thank you for letting me use your image.")
