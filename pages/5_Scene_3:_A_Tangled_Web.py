dbc4acd2f3cd756b0606ba7492b51b5782a9a29d
alistairwalsh <alistair.walsh@me.com>
2024-08-25 13:24:45 +1000

    tatiana pictures

--- pages/5_Scene_3:_A_Tangled_Web.py
+++ pages/5_Scene_3:_A_Tangled_Web.py
@@ -3,11 +3,11 @@ import streamlit.components.v1 as components
 
 st.title('A Tangled Web')
 
-st.image('images/tatiana3.png')
+st.image('images/character_headshots/tatiana3.png')
 
 with open('text/scene_3.txt') as infile:
     st.write(infile.read())
 
 st.video('https://youtu.be/kRtV9rHn-mo')
 
-st.image('images/tatiana4.png')
+st.image('images/character_headshots/tatiana4.png')
